5768edd01ed42227589ba87176e0aa4a96e068fb
Sagar karwa <153356716+Sagarkarwa10@users.noreply.github.com>
2025-10-04 18:29:40 +0530

    Add files via upload
    assignment 3 commits

---
+++ math.py
@@ -0,0 +1,14 @@
+from cmath import e
+
+import math;
+
+
+number=int(input("Enter a number: "))
+sqrt=math.sqrt(number)
+logs=math.log(number,e)
+sine=math.sin(number)
+
+
+print("Square root:",sqrt)
+print("Logarithm:",logs)
+print("Sine:",sine)
